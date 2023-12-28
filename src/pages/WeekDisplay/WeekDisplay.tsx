9773c63685b7c5e75d7bb489bee6ffc73fb38b66
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-12-28 15:14:19 +1100

    Adds weeks array creation logic

--- src/pages/WeekDisplay/WeekDisplay.tsx
+++ src/pages/WeekDisplay/WeekDisplay.tsx
@@ -1,7 +1,9 @@
+import * as _ from "underscore";
 import Week from "../../components/Week/Week";
 import Modal from "../../components/Modal/Modal";
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { splitIntoWeeks } from "../../utils/week-utils";
 import { getMonthData, getCurrentMonth } from "../../utils/month-utils";
 
 const WeekDisplay = () => {
@@ -20,14 +22,9 @@ const WeekDisplay = () => {
 
   const month = getMonthData(monthIndex);
 
-  const weeks = [
-    [1, 2, 3, 4, 5, 6, 7],
-    [8, 9, 10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19, 20, 21],
-    [22, 23, 24, 25, 26, 27, 28],
-    [29, 30, 31],
-  ];
+  const dates = _.range(1, month.numberOfDays + 1);
 
+  const weeks = splitIntoWeeks(dates);
   const weekNum = Math.floor((date - 1) / 7);
   const week = weeks[weekNum];
 
