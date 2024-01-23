76b2b4e32515a199f076cc471c93f7039fc2bd1b
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-01-23 17:41:30 +1100

    Adds basic form handling

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -1,10 +1,35 @@
 import styles from "./EventForm.module.scss";
+import { useRef } from "react";
 import Input from "../Input/Input";
 import Select from "../Select/Select";
 
 const EventForm = () => {
+  const formRef = useRef(null);
+
+  const events = new Array();
+
+  const submitForm = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const eventForm = formRef.current!;
+    const eventData = new FormData(eventForm);
+
+    const newEvent = {
+      eventName: eventData.get("eventName"),
+      startTime: eventData.get("startTime"),
+      endTime: eventData.get("endTime"),
+      location: eventData.get("location"),
+      address: eventData.get("address"),
+      url: eventData.get("url"),
+      label: eventData.get("label"),
+    };
+
+    events.push(newEvent);
+    console.log(events);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitForm} ref={formRef}>
       <div className={styles.form__error}>
         Error: Event start or end times cannot be in the past
       </div>
