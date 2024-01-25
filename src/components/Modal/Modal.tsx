f6b7ded9a35a5bb29e6443789bc9abcae0cdc125
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-01-25 17:08:13 +1100

    Updated create event form heading and styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,15 +2,14 @@ import styles from "./Modal.module.scss";
 import EventForm from "../EventForm/EventForm";
 
 interface ModalProps {
-  date: string;
   closeModal: () => void;
 }
 
-const Modal = ({ date, closeModal }: ModalProps) => {
+const Modal = ({ closeModal }: ModalProps) => {
   return (
     <section className={styles.modal}>
       <div className={styles.modal__header}>
-        <h3 className={styles.header__heading}>{date}</h3>
+        <h3 className={styles.header__heading}>Create A New Event</h3>
         <button className={styles.header__closeButton} onClick={closeModal}>
           x
         </button>
