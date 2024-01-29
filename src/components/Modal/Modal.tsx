170de8464e71df3d075ad885087c55bce8fcd96b
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2024-01-29 14:01:23 +1100

    Adds responsive modal styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,13 +8,15 @@ interface ModalProps {
 const Modal = ({ closeModal }: ModalProps) => {
   return (
     <section className={styles.modal}>
-      <div className={styles.modal__header}>
-        <h3 className={styles.header__heading}>Create A New Event</h3>
-        <button className={styles.header__closeButton} onClick={closeModal}>
-          x
-        </button>
-      </div>
-      <EventForm />
+      <section className={styles.modal__content}>
+        <div className={styles.modal__header}>
+          <h3 className={styles.header__heading}>Create A New Event</h3>
+          <button className={styles.header__closeButton} onClick={closeModal}>
+            x
+          </button>
+        </div>
+        <EventForm />
+      </section>
     </section>
   );
 };
